23010d7a95c4f2cf73bfb71920a3b23917bc3f72
Yuhan224 <fir3panic@gmail.com>
2023-11-01 09:23:43 -0400

    dsfdfsdf

---
+++ notes.js
@@ -0,0 +1,29 @@
+/*for(i=0; i<= 7; i++){
+    console.log(i);
+}
+money= 100;
+while( money > 0){
+    console.log(money);
+    money--;
+}
+*/
+
+//find item in array 
+/*function linearSearch (num, arr,) {
+    let found;
+    for(let i =0; i< arr.length; i++){
+        if(arr[i] === num){
+            found= arr[i];
+            break;
+        }else {
+            console.log ("SIKEEEEEEEEE");
+        }
+    }
+    if(!found){
+        console.log("womp womp")
+    }
+    console.log(found);
+return found;
+}
+linearSearch(200, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23])
+*/
